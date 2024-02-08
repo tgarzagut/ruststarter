9994ef2fb2a1b40edbece78fee477c887bff9e3c
tgarzagut <102454563+tgarzagut@users.noreply.github.com>
2024-02-08 00:11:07 +0000

    In class Assignment.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
+//Question 1
+// fn concat_strings(s1: &String, s2: &String) -> String {
+//     let concat = format!("{}{}", s1, s2);
+//     return concat;
+     
+// }
+    
+// fn main() {
+//     let s1 = String::from("Hello, ");
+//     let s2 = String::from("World!");
+//     let result = concat_strings(&s1, &s2);
+//     println!("{}", result); // Should print: "Hello, World!"
+// }
 
-mod implement;
+//Question 2
+// fn clone_and_modify(s: &String) -> String {
+//     let sclone = s.clone();
+//     let newstring = format!("{}you.", sclone);
+//     return newstring;
+// }
 
-fn main() {
-    println!("Run functions");
-    println!("{} and {}", implement::add(10,15), implement::subtract(15,10));
-}
+// fn main() {
+//     let s = String::from("Hello, ");
+//     let modified = clone_and_modify(&s);
+//     println!("Original: {}", s); // Should print: "Original: Hello, "
+//     println!("Modified: {}", modified); // Should print: "Modified: Hello, World!"
+// }
+
+//Question 3
+// #[allow(unused_variables, unused_mut)]
+// fn sum(total: &mut i32, low: i32, high: i32) {
+//     for i in low..=high {
+//         *total +=i;
+//     }
+// }
+
+// fn main(){
+//     let mut total = 0;
+//     let low:i32 = 0;
+//     let high:i32 = 100;
+//     sum(&mut total, low, high);
+//     println!("Total: {}", total);
+// }
+    
